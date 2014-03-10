315383b121244983ee7020f251722f78008d44af
Odin Dutton <odindutton@gmail.com>
2014-03-10 10:35:47 +1000

    Add root path

--- app/human.rb
+++ app/human.rb
@@ -6,6 +6,16 @@ class Human < Sinatra::Base
     content_type :json
   end
 
+  get '/' do
+    {
+      _links: {
+        location: {
+          href: "#{request.url.sub(%r{#{request.path}$}, '')}/location.json"
+        }
+      }
+    }.to_json
+  end
+
   get '/location.json' do
     Location.new.location.to_json
   end
